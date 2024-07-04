8581eb0004b53c4a6c9141037151bd7feb6ab01e
DaYascaribay <david-ldu03@hotmail.com>
2024-07-04 18:00:05 -0500

    Se arreglo un poco la pagina principal

--- src/components/ObservarGastosTablesmes/index.jsx
+++ src/components/ObservarGastosTablesmes/index.jsx
@@ -18,7 +18,7 @@ export default function ObservarGastosTablemes({
             className={`${props.className} flex sm:flex-col justify-center items-center border-gray-300 border-[0.5px] border-solid flex-1 rounded-lg`}
         >
             <div className="outline-b-[0.5] outline-t-[0.5] flex flex-col bg-white-a700 px-7 py-[38px] outline outline-gray-300">
-                <img src="images/img_arrow_up_right.svg" alt="image" className="h-[24px] w-[24px]" />
+                <Img src="images/img_arrow_up_right.svg" alt="image" className="h-[24px] w-[24px]" />
             </div>
             <div className="outline-b-[0.5] outline-t-[0.5] flex flex-1 flex-col items-start justify-center gap-0.5 bg-white-a700 px-4 py-[18px] outline outline-gray-300 sm:self-stretch">
                 <Text as="p">{summarytext}</Text>
@@ -32,7 +32,7 @@ export default function ObservarGastosTablemes({
                 </Button>
             </div>
             <div className="outline-b-[0.5] outline-t-[0.5] flex justify-center bg-white-a700 p-10 outline outline-gray-300"> 
-                <img src="images/img_slider.svg" alt="image" className="h-[22px] w-[92%]" />
+                <Img src="images/img_slider.svg" alt="image" className="h-[22px] w-[92%]" />
             </div>
         </div>
     );
